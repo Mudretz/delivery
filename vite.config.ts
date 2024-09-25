7c20eaff29af639876e6975ae6d008e9808be3d3
Gungaev Zhamso <zham.drag@gmail.com>
2024-09-25 22:19:56 +0800

    настройка приложения и установка зависимостей

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [tsconfigPaths(), react(), svgr()],
+    test: {
+        globals: true,
+        environment: "jsdom",
+        setupFiles: "./tests/setup.ts",
+    },
+});
